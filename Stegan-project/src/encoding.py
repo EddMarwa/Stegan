37c2ac13c8b2344d725f5edb00440beac01e8d07
EddMarwa <149029644+EddMarwa@users.noreply.github.com>
2024-09-18 08:15:02 +0300

    encoding

---
+++ Stegan-project/src/encoding.py
@@ -0,0 +1,20 @@
+from PIL import Image
+
+def encode_message(image_path, message, output_path):
+    img = Image.open(image_path)
+    binary_message = ''.join([format(ord(char), '08b') for char in message])
+    img_data = img.getdata()
+    
+    new_data = []
+    msg_index = 0
+    for pixel in img_data:
+        if msg_index < len(binary_message):
+            new_pixel = list(pixel)
+            new_pixel[0] = (pixel[0] & ~1) | int(binary_message[msg_index])
+            new_data.append(tuple(new_pixel))
+            msg_index += 1
+        else:
+            new_data.append(pixel)
+    
+    img.putdata(new_data)
+    img.save(output_path)
