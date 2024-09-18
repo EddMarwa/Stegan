97ae89191dfa2cf1e4a22c0b4e76c47204bbb08d
EddMarwa <149029644+EddMarwa@users.noreply.github.com>
2024-09-18 08:15:25 +0300

    decoding code

---
+++ Stegan-project/src/decoding.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+def decode_message(image_path):
+    img = Image.open(image_path)
+    img_data = img.getdata()
+    
+    binary_message = ""
+    for pixel in img_data:
+        binary_message += str(pixel[0] & 1)
+    
+    chars = [binary_message[i:i+8] for i in range(0, len(binary_message), 8)]
+    decoded_message = ''.join([chr(int(char, 2)) for char in chars])
+    
+    return decoded_message
