14e9c78927ab9bd20d2bda6a89feeb8be3995da2
EddMarwa <149029644+EddMarwa@users.noreply.github.com>
2024-09-18 11:11:43 +0300

    final commit

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from encoding import encode_message
 from decoding import decode_message
 
-
 def main():
     choice = input("Enter 'e' to encode or 'd' to decode: ")
     if choice == 'e':
-        image_path = input("Enter the image path: ")
+        image_path = input("Enter the image path (e.g., images/test_image.png): ")
         message = input("Enter the message to hide: ")
-        output_path = input("Enter the output image path: ")
+        output_path = input("Enter the output image path (e.g., images/output_image.png): ")
         encode_message(image_path, message, output_path)
     elif choice == 'd':
-        image_path = input("Enter the image path to decode: ")
+        image_path = input("Enter the image path to decode (e.g., images/output_image.png): ")
         decoded_message = decode_message(image_path)
         print(f"Decoded message: {decoded_message}")
     else:
-        print("Invalid choice.")
+        print("Invalid choice. Please enter 'e' to encode or 'd' to decode.")
 
 if __name__ == "__main__":
     main()
