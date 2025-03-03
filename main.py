c257315b0a1ab92aed8ace9db715ba6b992f98d3
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-03-03 13:02:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 from encoding import encode_message
 from decoding import decode_message
-# Function to handle the encoding process
+# Function to handle the encoding processes
 def encode():
     image_path = filedialog.askopenfilename(title="Select Image for Encoding")
     if not image_path:
